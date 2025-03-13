566a7af6f86c9931a5e8d533b7ca132dadcedfee
Aravind280306 <aravindv2803@gmail.com>
2025-03-13 14:37:33 +0530

    Update mathrix.c

--- mathrix.c
+++ mathrix.c
@@ -1,180 +1,224 @@
+#include<stdio.h>
+#include<stdlib.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-int** allocatememory(int rows, int cols)
+void oneallocate(int m,int n,int **ptr1)
 {
-    int **matrix = (int **)malloc(rows * sizeof(int *));
-    for (int i = 0; i < rows; i++)
+    for(int i=0;i<m;i++)
     {
-        matrix[i] = (int *)malloc(cols * sizeof(int));
-    }
-    return matrix;
+        ptr1[i] = (int *) malloc(sizeof(int) * n);
+    }   
 }
-void freememory(int **matrix, int rows)
+void twoallocate(int m,int n,int **ptr1,int **ptr2)
 {
-    for (int i = 0; i < rows; i++)
+    for(int i=0;i<m;i++)
     {
-        free(matrix[i]);
+        ptr1[i] = (int *) malloc(sizeof(int) * n);
+        ptr2[i] = (int *) malloc(sizeof(int) * n);
     }
-    free(matrix);
 }
-void input(int **matrix, int rows, int cols)
+void add(int m, int n,int **ptr1,int **ptr2)
 {
-    printf("ENTER THE ELEMENTS OF THE MATRIX:\n");
-    for (int i = 0; i < rows; i++)
+   for(int i=0;i<m;i++)
     {
-        for (int j = 0; j < cols; j++)
+        for(int j=0 ;j<n; j++)
         {
-            scanf("%d", &matrix[i][j]);
-        }
-    }
-
-}
-void displayMatrix(int **matrix, int rows, int cols)
-{
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
-        {
-            printf("%d\t", matrix[i][j]);
+            printf("%d\t", ptr1[i][j] + ptr2[i][j]);
         }
         printf("\n");
     }
 }
-void addition(int **mat_1, int **mat_2, int **ans, int rows, int cols)
+void sub(int m, int n,int **ptr1,int **ptr2)
 {
-    for (int i = 0; i < rows; i++)
+   for(int i=0;i<m;i++)
     {
-        for (int j = 0; j < cols; j++)
+        for(int j=0 ;j<n; j++)
         {
-            ans[i][j] = mat_1[i][j] + mat_2[i][j];
+            printf("%d\t", ptr1[i][j] - ptr2[i][j]);
         }
+        printf("\n");
     }
 }
-void subtraction(int **mat_1, int **mat_2, int **ans, int rows, int cols)
+void mul(int m, int n,int m1,int n1,int **ptr1,int **ptr2)
 {
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < cols; j++)
+    int **result = malloc(sizeof(int *) * n1);
+    oneallocate(m,n1,result);
+    int sum;
+    
+        for(int i=0;i<m;i++)
         {
-            ans[i][j] = mat_1[i][j] - mat_2[i][j];
-        }
-    }
-}
-void multiplication(int **mat_1, int **mat_2, int **ans, int r1, int c1, int c2)
-{
-    for (int i = 0; i < r1; i++)
-    {
-        for (int j = 0; j < c2; j++)
-        {
-            ans[i][j] = 0;
-            for (int k = 0; k < c1; k++)
-            {
-                ans[i][j] += mat_1[i][k] * mat_2[k][j];
+           for(int j=0 ;j<m; j++)
+           {    
+                sum = 0;
+                for(int k=0 ; k<n1 ; k++)
+                {
+                    sum += ptr1[i][k] * ptr2[k][j];
+                }
+                result[i][j] = sum;
+                printf("%d   ",result[i][j]);
             }
+            printf("\n");
         }
-    }
 }
-void transpose(int **matrix, int **ans, int rows, int cols)
+void transpose(int m,int n,int **ptr1)
 {
-    for (int i = 0; i < cols; i++)
+  for(int i=0;i<m;i++)
+      {
+          for(int j=0;j<n;j++)
+          {
+              printf("%d\t",ptr1[i][j]);
+          }
+
+          printf("\n");
+      }
+    printf("-----------------------------------------\n");
+    for(int i=0;i<n;i++)
     {
-        for (int j = 0; j < rows; j++)
+        for(int j=0;j<m;j++)
         {
-            ans[i][j] = matrix[j][i];
+            printf("%d\t",ptr1[j][i]);
         }
+        printf("\n");
     }
 }
+
 int main()
 {
-    int option, r1, c1, r2, c2;
-    int **mat_1, **mat_2, **ans;
-    while (1)
+    
+    int row,col,row1,col1,choice;
+    while(1)
     {
-        printf("***************************\n");
-        printf("*MATRIX CALCULATION MENU: *\n");
-        printf("*1. ADDITION              *\n");
-        printf("*2. SUBTRACTION           *\n");
-        printf("*3. MULTIPLICATION        *\n");
-        printf("*4. TRANSPOSE             *\n");
-        printf("*5. END                   *\n");
-        printf("***************************\n");
-        printf("ENTER YOUR OPTION :       \n");
-        scanf("%d", &option);
-        if (option == 5)
-        {
-            printf("PROGRAM ENDS......\n");
-            break;
-        }
-        printf("ENTER NO. OF ROWS AND COLUMNS FOR FIRST MATRIX: ");
-        scanf("%d %d", &r1, &c1);
-        mat_1 = allocatememory(r1, c1);
-        input(mat_1, r1, c1);
-        if (option != 4)
-        {
-            printf("ENTER NO. OF ROWS AND COLUMNS FOR SECOND MATRIX: ");
-            scanf("%d %d", &r2, &c2);
+        choice =0;
+        printf("-----------------------------------------\n");
+        printf(" 1.ADDITION \n 2.SUBRACTION \n 3.TRANSPOSE \n 4.MULTIPLICATION \n 5.EXIT \n");
+        printf("Enter Your Choice : ");
+        scanf("%d",&choice);
+        
+        int **ptr1; 
+        int **ptr2;
+            
 
-            mat_2 = allocatememory(r2, c2);
-            input(mat_2, r2, c2);
-        }
-        switch (option)
+        switch(choice)
         {
             case 1:
-                if (r1 != r2 || c1 != c2)
+                printf("Enter the value of row     : ");
+                scanf("%d",&row);
+                printf("Enter the value of column  : ");
+                scanf("%d",&col);
+                ptr1 = malloc(sizeof(int *) * row);
+                ptr2 = malloc(sizeof(int *) * row);
+                twoallocate(row,col,ptr1,ptr2);
+                printf("Enter the elements of matrix A : ");
+                for(int i=0;i<row;i++)
                 {
-                    printf("DIMENSTIONS MUST BE SAME\n");
-                } else
-                {
-                    ans = allocatememory(r1, c1);
-                    addition(mat_1, mat_2, ans, r1, c1);
-                    printf("ANSWER FOR ADDITION:\n");
-                    displayMatrix(ans, r1, c1);
-                    freememory(ans, r1);
+                    for(int j=0 ;j<col; j++)
+                    {
+                        scanf("%d",&ptr1[i][j]);
+                    }
                 }
+                printf("Enter the elements of matrix B : ");
+                for(int i=0;i<row;i++)
+                {
+                    for(int j=0 ;j<col; j++)
+                    {
+                        scanf("%d",&ptr2[i][j]);
+                    }
+                }
+                printf("-----------------------------------------\n");
+                add(row,col,ptr1,ptr2);
                 break;
             case 2:
-                if (r1 != r2 || c1 != c2)
+                printf("Enter the value of row      : ");
+                scanf("%d",&row);
+                printf("Enter the value of column    : ");
+                scanf("%d",&col);
+                ptr1 = malloc(sizeof(int *) * row);
+                ptr2 = malloc(sizeof(int *) * row);
+                twoallocate(row,col,ptr1,ptr2);
+                printf("Enter the elements of matrix A  : ");
+                for(int i=0;i<row;i++)
                 {
-                    printf("DIMENSTIONS MUST BE SAME \n");
-                } else
-                {
-                    ans = allocatememory(r1, c1);
-                    subtraction(mat_1, mat_2, ans, r1, c1);
-                    printf("ANSWER FOR SUBTRACTION:\n");
-                    displayMatrix(ans, r1, c1);
-                    freememory(ans, r1);
+                    for(int j=0 ;j<col; j++)
+                    {
+                        scanf("%d",&ptr1[i][j]);
+                    }
                 }
+                printf("Enter the elements of matrix B  : ");
+                for(int i=0;i<row;i++)
+                {
+                    for(int j=0 ;j<col; j++)
+                    {
+                        scanf("%d",&ptr2[i][j]);
+                    }
+                }
+                printf("-----------------------------------------\n");
+                sub(row,col,ptr1,ptr2);
                 break;
             case 3:
-                if (c1 != r2)
+                printf("Enter the value of row : ");
+                scanf("%d",&row);
+                printf("Enter the value of col  : ");
+                scanf("%d",&col);
+                ptr1 = malloc(sizeof(int *) * row);
+                oneallocate(row,col,ptr1);
+                printf("Enter the elements of matrix A  : ");
+                for(int i=0;i<row;i++)
                 {
-                    printf("COLUMNS OF FIRST MATRIX MUST BE EQUAL TO ROWS OF SECOND MATRIX\n");
-                } else
-                {
-                    ans = allocatememory(r1, c2);
-                    multiplication(mat_1, mat_2, ans, r1, c1, c2);
-                    printf("ANSWER FOR MULTIPILCATION:\n");
-                    displayMatrix(ans, r1, c2);
-                    freememory(ans, r1);
+                    for(int j=0 ;j<col; j++)
+                    {
+                        scanf("%d",&ptr1[i][j]);
+                    }
                 }
+                printf("-----------------------------------------\n");
+                transpose(row,col,ptr1);
                 break;
             case 4:
-                ans = allocatememory(c1, r1);
-                transpose(mat_1, ans, r1, c1);
-                printf("TRANSPOSE OF THE MATRIX:\n");
-                displayMatrix(ans, c1, r1);
-                freememory(ans, c1);
+                printf("Enter the value of first row      : ");
+                scanf("%d",&row); 
+                printf("Enter the value of first  column   :");
+                scanf("%d",&col);
+                printf("Enter the value of second row  :");
+                scanf("%d",&row1);
+                printf("Enter the value of second column  : ");
+                scanf("%d",&col1);
+                ptr1 = malloc(sizeof(int *) * col);
+                ptr2 = malloc(sizeof(int *) * col1);
+                oneallocate(row,col,ptr1);
+                oneallocate(row1,col1,ptr2);
+                
+                if(col != row1)
+                {
+                    printf("Try Again\n");
+                    break;
+                    
+                }
+                else
+                {
+                    printf("Enter the elements of matrix A  ");
+                    for(int i=0;i<row;i++)
+                    {
+                        for(int j=0 ;j<col; j++)
+                        {
+                            scanf("%d",&ptr1[i][j]);
+                        }
+                    }
+                    printf("Enter the elements of matrix B  ");
+                    for(int i=0;i<row1;i++)
+                    {
+                        for(int j=0 ;j<col1; j++)
+                        {
+                            scanf("%d",&ptr2[i][j]);
+                        }
+                    }
+                    printf("-----------------------------------------\n");
+                    mul(row,col,row1,col1,ptr1,ptr2);
+                }
                 break;
+            case 5:
+                printf("THANK YOU.............");
+                return 0;
             default:
-                printf("INVALID , PLEASE TRY AGAIN\n");
+                printf("Invalid Choice");
                 break;
-        }
-        freememory(mat_1, r1);
-        if (option != 4) {
-            freememory(mat_2, r2);
         }
     }
-    return 0;
 }
-    
