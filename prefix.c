570b12a6071188f4331bea0244f6c0b2c76ba3aa
ARAVIND <aravindv2803@gmail.com>
2025-03-27 07:39:25 +0530

    Update prefix.c

--- prefix.c
+++ prefix.c
@@ -2,192 +2,184 @@
 #include<string.h>
 #include<stdbool.h>
 
-bool isclo(char c)
+bool isclose(char c)
 {
-    if(c == ')' || c == '}' || c == ']' )
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+if(c == ')' || c == '}' || c == ']' )
+{
+return true;
 }
-bool isope(char c)
+else
 {
-    if(c == '(' || c == '{' || c == '[' )
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+return false;
+}
+}
+bool isopen(char c)
+{
+if(c == '(' || c == '{' || c == '[' )
+
+    return true;
+
+else
+
+    return false;
+
 }
 
 bool isfull(char s[],int *top,int max)
 {
-    if(*top == max-1)
-    {
-        return false;
-    }
-    else 
-    {
-        return true;
-    }
+if(*top == max-1)
+    return false;
+else
+    return true;
 }
 
 bool isempty(char s[],int *top)
 {
-    if(*top == -1)
-    {
-        return false;
-    }
-    else 
-    {
-        return true;
-    }
+if(*top == -1)
+    return false;
+else
+    return true;
 }
 
 void push(char s[],int *top,int max,int x)
 {
-    if(isfull(s,top,max))
-    {
-        (*top)++;
-        s[*top] = x;
-    }
-    
+if(isfull(s,top,max))
+{
+(*top)++;
+s[*top] = x;
+}
+
 }
 
 char pop(char s[],int *top,int max)
 {
-    if(isempty(s,top))
-    {
-        char temp = s[*top];
-        (*top)--;
-        return temp;
-    }
+if(isempty(s,top))
+{
+char temp = s[*top];
+(*top)--;
+return temp;
+}
 }
 
 char peektop(char s[],int *top)
 {
-    if(isempty(s,top))
-    {
-        return s[*top];
-    }
+if(isempty(s,top))
+{
+return s[*top];
+}
 }
 
 
 
-int iss(char e)
+int ins(char e)
 {
-    if(e == '$')
-    {
-        return 4;
-    }
-    else if (e == '*' || e == '/')          
-    {
-        return 3;
-    }
-    else if (e == '+' || e == '-')
-    {
-        return 2;
-    }
-    else if (e == ')')
-    {
-        return 0;
-    }
+if(e == '$')
+{
+return 4;
+}
+else if (e == '*' || e == '/')
+{
+return 3;
+}
+else if (e == '+' || e == '-')
+{
+return 2;
+}
+else if (e == ')')
+{
+return 0;
+}
 }
 
-int iso(char e)
+int ico(char e)
 {
-    if(e == '$')
-    {
-        return 5;
-    }
-    else if (e == '*' || e == '/')          
-    {
-        return 3;
-    }
-    else if (e == '+' || e == '-')
-    {
-        return 2;
-    }
-    else if (e == ')')
-    {
-        return 5;
-    }
-    else if (e == '(')
-    {
-        return 1;
-    }
+if(e == '$')
+{
+return 5;
+}
+else if (e == '*' || e == '/')
+{
+return 3;
+}
+else if (e == '+' || e == '-')
+{
+return 2;
+}
+else if (e == ')')
+{
+return 5;
+}
+else if (e == '(')
+{
+return 1;
+}
 }
 
 bool pre(char f, char d)
 {
-    if(iss(f) > iso(d))
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+if(ins(f) > ico(d))
+{
+return true;
+}
+else
+{
+return false;
+}
 }
 
 void res(char s[],char r[])
 {
-    char sta[100];   
-    int k = 0;
-    int top = -1;
-    char sym;
-    int max = 99;
-    for(int i = strlen(s)-1 ; i >= 0 ; i--)
-    {
-        if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
-        {
-            r[k] = s[i];
-            k++;
-        }
-        else
-        {
-            while(isempty(sta,&top) && !(isclo(peektop(sta,&top))) && pre(peektop(sta,&top),s[i]))
-            {
-                sym = pop(sta,&top,max);
-                r[k] = sym;
-                k++; 
-            }
+char sta[100];
+int k = 0;
+int top = -1;
+char sym;
+int max = 99;
+for(int i = strlen(s)-1 ; i >= 0 ; i--)
+{
+if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+{
+r[k] = s[i];
+k++;
+}
+else
+{
+while(isempty(sta,&top) && !(isclose(peektop(sta,&top))) && pre(peektop(sta,&top),s[i]))
+{
+sym = pop(sta,&top,max);
+r[k] = sym;
+k++;
+}
 
-            if(!(isope(s[i])))
-            {
-                push(sta,&top,max,s[i]);
-            }
-            else
-            {
-                sym = pop(sta,&top,max);
-            }
-        }
-    }
-    while(isempty(sta,&top))
-    {
-        sym = pop(sta,&top,max);
-        r[k] = sym;
-        k++;
-    }
+if(!(isopen(s[i])))
+{
+push(sta,&top,max,s[i]);
+}
+else
+{
+sym = pop(sta,&top,max);
+}
+}
+}
+while(isempty(sta,&top))
+{
+sym = pop(sta,&top,max);
+r[k] = sym;
+k++;
+}
 }
 
 int main()
 {
-    char s[100];
-    printf("enter the string");
-    scanf("%s",s);
-    char r[100];
-    res(s,r);
-    int  k = strlen(r);
-    for(int i = k-1 ; i>=0 ; i--)
-    {
-        printf("%c",r[i]);
-    }
+char s[100];
+printf("enter the string");
+scanf("%s",s);
+char r[100];
+res(s,r);
+int k = strlen(r);
+for(int i = k-1 ; i>=0 ; i--)
+{
+printf("%c",r[i]);
+}
 
-    
+
 }
