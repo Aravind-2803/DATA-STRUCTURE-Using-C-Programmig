d37a51a0b8d1726228ba1d9d9039fcf48687b609
ARAVIND <aravindv2803@gmail.com>
2025-04-13 08:05:10 +0530

    Update Linear Sort.c

--- Sorting/Linear Sort.c
+++ Sorting/Linear Sort.c
@@ -25,6 +25,12 @@ int main()
 {
   int data[] = {9, 5, 1, 4, 3};
   int size = sizeof(data) / sizeof(data[0]);
+  printf("Sorted Array in Ascending Order:\n");
+  for (int i = 0; i < size; ++i) 
+  {
+    printf("%d  ", data[i]);
+  }
+  printf("\n");
   insertionSort(data, size);
   printf("Sorted array in ascending order:\n");
   printArray(data, size);
