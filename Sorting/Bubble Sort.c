489b4a5b7c82ccebb5a3fdb4c6f2db6f21a01f59
ARAVIND <aravindv2803@gmail.com>
2025-04-13 08:04:50 +0530

    Update Bubble Sort.c

--- Sorting/Bubble Sort.c
+++ Sorting/Bubble Sort.c
@@ -18,6 +18,12 @@ int main()
 {
   int data[] = {-2, 45, 0, 11, -9};
   int size = sizeof(data) / sizeof(data[0]);
+  printf("Unsorted Array :\n");
+  for (int i = 0; i < size; ++i) 
+  {
+    printf("%d  ", data[i]);
+  }
+  printf("\n");
   bubbleSort(data, size);
   printf("Sorted Array in Ascending Order:\n");
   for (int i = 0; i < size; ++i) 
